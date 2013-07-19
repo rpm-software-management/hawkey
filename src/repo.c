rpm-software-management/hawkey

commit 68ebca4a80aec636d30a9fd4fb9aa2d9bf9a8eca

Add updateinfo support to hawkey, using the existing parser in libsolv

src/repo.c
@@ -45,6 +45,9 @@ repo_update_state(HyRepo repo, enum _hy_repo_repodata which,
     case _HY_REPODATA_PRESTO:
 	repo->state_presto = state;
 	return;
+    case _HY_REPODATA_UPDATEINFO:
+	repo->state_updateinfo = state;
+	return;
     default:
 	assert(0);
     }
@@ -59,6 +62,8 @@ repo_get_repodata(HyRepo repo, enum _hy_repo_repodata which)
 	return repo->filenames_repodata;
     case _HY_REPODATA_PRESTO:
 	return repo->presto_repodata;
+    case _HY_REPODATA_UPDATEINFO:
+	return repo->updateinfo_repodata;
     default:
 	assert(0);
 	return 0;
@@ -74,6 +79,9 @@ repo_set_repodata(HyRepo repo, enum _hy_repo_repodata which, Id repodata)
 	return;
     case _HY_REPODATA_PRESTO:
 	repo->presto_repodata = repodata;
+	return;
+    case _HY_REPODATA_UPDATEINFO:
+	repo->updateinfo_repodata = repodata;
 	return;
     default:
 	assert(0);
@@ -117,6 +125,10 @@ hy_repo_set_string(HyRepo repo, int which, const char *str_val)
 	solv_free(repo->presto_fn);
 	repo->presto_fn = solv_strdup(str_val);
 	break;
+    case HY_REPO_UPDATEINFO_FN:
+	solv_free(repo->updateinfo_fn);
+	repo->updateinfo_fn = solv_strdup(str_val);
+	break;
     default:
 	assert(0);
     }
@@ -136,6 +148,8 @@ hy_repo_get_string(HyRepo repo, int which)
 	return repo->filelists_fn;
     case HY_REPO_PRESTO_FN:
 	return repo->presto_fn;
+    case HY_REPO_UPDATEINFO_FN:
+	return repo->updateinfo_fn;
     default:
 	assert(0);
     }

src/repo.h
@@ -33,7 +33,8 @@ enum _hy_repo_param_e {
     HY_REPO_MD_FN,
     HY_REPO_PRESTO_FN,
     HY_REPO_PRIMARY_FN,
-    HY_REPO_FILELISTS_FN
+    HY_REPO_FILELISTS_FN,
+    HY_REPO_UPDATEINFO_FN
 };
 
 HyRepo hy_repo_create(const char *name);

src/repo_internal.h
@@ -42,18 +42,22 @@ struct _HyRepo {
     char *primary_fn;
     char *filelists_fn;
     char *presto_fn;
+    char *updateinfo_fn;
     enum _hy_repo_state state_main;
     enum _hy_repo_state state_filelists;
     enum _hy_repo_state state_presto;
+    enum _hy_repo_state state_updateinfo;
     Id filenames_repodata;
     Id presto_repodata;
+    Id updateinfo_repodata;
     unsigned char checksum[CHKSUM_BYTES];
     Repo *libsolv_repo;
 };
 
 enum _hy_repo_repodata {
     _HY_REPODATA_FILENAMES,
-    _HY_REPODATA_PRESTO
+    _HY_REPODATA_PRESTO,
+    _HY_REPODATA_UPDATEINFO
 };
 
 HyRepo hy_repo_link(HyRepo repo);

src/sack.c
@@ -36,6 +36,7 @@
 #include <solv/repo.h>
 #include <solv/repo_deltainfoxml.h>
 #include <solv/repo_repomdxml.h>
+#include <solv/repo_updateinfoxml.h>
 #include <solv/repo_rpmmd.h>
 #include <solv/repo_rpmdb.h>
 #include <solv/repo_solv.h>
@@ -312,6 +313,14 @@ static int
 load_presto_cb(Repo *repo, FILE *fp)
 {
     if (repo_add_deltainfoxml(repo, fp, 0))
+	return HY_E_LIBSOLV;
+    return 0;
+}
+
+static int
+load_updateinfo_cb(Repo *repo, FILE *fp)
+{
+    if (repo_add_updateinfoxml(repo, fp, 0))
 	return HY_E_LIBSOLV;
     return 0;
 }
@@ -762,6 +771,13 @@ hy_sack_load_yum_repo(HySack sack, HyRepo repo, int flags)
 	    goto finish;
 	if (repo->state_presto == _HY_LOADED_FETCH && build_cache)
 	    retval = write_ext(sack, repo, _HY_REPODATA_PRESTO, HY_EXT_PRESTO);
+    }
+    if (flags & HY_LOAD_UPDATEINFO) {
+	retval = load_ext(sack, repo, _HY_REPODATA_UPDATEINFO,
+			  HY_EXT_UPDATEINFO, HY_REPO_UPDATEINFO_FN,
+			  load_updateinfo_cb);
+	if (retval)
+	    goto finish;
     }
  finish:
     if (retval) {

src/sack.h
@@ -35,7 +35,8 @@ enum _hy_sack_sack_create_flags {
 enum _hy_sack_repo_load_flags {
     HY_BUILD_CACHE	= 1 << 0,
     HY_LOAD_FILELISTS	= 1 << 1,
-    HY_LOAD_PRESTO	= 1 << 2
+    HY_LOAD_PRESTO	= 1 << 2,
+    HY_LOAD_UPDATEINFO	= 1 << 3
 };
 
 HySack hy_sack_create(const char *cache_path, const char *arch,

src/types.h
@@ -47,6 +47,7 @@ typedef int (*hy_solution_callback)(HyGoal goal, void *callback_data);
 #define HY_SYSTEM_RPMDB "/var/lib/rpm/Packages"
 #define HY_CMDLINE_REPO_NAME "@commandline"
 #define HY_EXT_FILENAMES "-filenames"
+#define HY_EXT_UPDATEINFO "-updateinfo"
 #define HY_EXT_PRESTO "-presto"
 
 #define HY_CHKSUM_MD5		1

tests/fixtures.c
@@ -137,6 +137,7 @@ void setup_yum_sack(HySack sack, const char *yum_repo_name)
     fail_if(hy_sack_load_yum_repo(sack, repo,
 				  HY_BUILD_CACHE |
 				  HY_LOAD_FILELISTS |
+				  HY_LOAD_UPDATEINFO |
 				  HY_LOAD_PRESTO));
     fail_unless(hy_sack_count(sack) == TEST_EXPECT_YUM_NSOLVABLES);
     hy_repo_free(repo);

tests/repos/yum/repodata/repomd.xml
@@ -33,4 +33,10 @@
   <size>883</size>
   <open-size>2352</open-size>
 </data>
+<data type="updateinfo">
+  <checksum type="sha256">d4da087881909fc614788002db1ecd918840a733b951363eaf86b4c37667b2ff</checksum>
+  <open-checksum type="sha256">e05cfe3d6116a9cf16ed0d9e1c9d2293d7d0d5f0c1187222dcdf62c1acefe410</open-checksum>
+  <location xml:base="blah" href="repodata/updateinfo.xml.gz"/>
+  <timestamp>1284438472</timestamp>
+</data>
 </repomd>

commit 0376b8a6443e1639eeecfdde8e14e81c37003951

Fix a tiny memory leak introduced in 68ebca4a80aec636d30a9fd4fb9aa2d9bf9a8eca

src/repo.c
@@ -169,5 +169,6 @@ hy_repo_free(HyRepo repo)
     solv_free(repo->primary_fn);
     solv_free(repo->filelists_fn);
     solv_free(repo->presto_fn);
+    solv_free(repo->updateinfo_fn);
     solv_free(repo);
 }